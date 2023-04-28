1c699facb932fb34897b2e14f631579d2fea4ff6
KanuriSathvika <117982917+KanuriSathvika@users.noreply.github.com>
2023-04-28 05:53:52 +0000

    user logged in  and cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,12 @@
 
 import asyncHandler from "../service/asyncHandler";
 import CustomError from "../utils/CustomError";
+import User from  "../models/user.schema" ;
+
+export const cookieOptions ={
+    expires: new Date(Date.now()+3*24*60*60*1000),
+    httpOnly:true
+}
 
 export const signUp =asyncHandler(async(req,res)=>{
     // get data from user
@@ -11,4 +17,32 @@ export const signUp =asyncHandler(async(req,res)=>{
     if(!name || !email || !password){
         throw new CustomError("Please add all fields",400)
     }
+
+    // lets add this data to database
+    // check if user already exists
+    const existingUser= await User.findOne({email})
+
+    if(existingUser){
+        throw new CustomError("User already exists",400)
+
+    }
+
+    const user=await User.create({
+        name,
+        email,
+        password 
+    })
+
+    const token = user.getJWTtoken()
+    // safety
+    user.password = undefined
+    
+    // store this token in user's cookie
+    res.cookie("token",token,cookieOptions)
+    // send back a response to user
+    res.status(20).json({
+        sucess:true,
+        token,
+        user
+    })
 })
